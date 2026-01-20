09f768d2920bb5d5c5ad5a1d856aa4edd8aad7a1
unknown <MithileshWatane11@gmail.com>
2026-01-20 19:38:54 +0530

    first commit

--- Healthcare/src/services/medicalAnalysisService.ts
+++ Healthcare/src/services/medicalAnalysisService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3002/api';
+const API_BASE_URL = 'https://healthcare-document.onrender.com/api';
 
 export interface AnalysisResponse {
     success: boolean;
